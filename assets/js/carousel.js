6c0982c7e5ae8aee099347de736576d997e78c3e
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2024-04-17 22:49:51 -0400

    Added js carousel
    Added javascript for a carousel feature to showcase demos. Added new CSS for experience and carousel.

---
+++ assets/js/carousel.js
@@ -0,0 +1,94 @@
+var indices = new Object();
+
+function setCarouselItem(carousel, items, buttons, i) {
+    // un-select all items
+    items.forEach(item => {
+        // Find the videos in the item and pause and restart them
+        const videos = item.querySelectorAll("video");
+        videos.forEach(video => {
+            video.pause();
+            video.currentTime = 0;
+            video.volume = 1.0;
+            video.muted = false;
+            video.playbackRate = 1.0;
+        });
+
+        // Remove the selected attribute from the item if the item has it
+        item.classList.remove("carousel_item-selected");
+    });
+    buttons.forEach(button => button.classList.remove("carousel_button-selected"));
+
+    items[i].classList.add("carousel_item-selected");
+    buttons[i].classList.add("carousel_button-selected");
+    indices[carousel] = i;
+}
+
+/* 
+    Set up each carousel 
+*/
+document.querySelectorAll(".carousel").forEach(carousel => {
+    // Add a new entry to the "hash table" of carousels
+    indices[carousel] = 0;
+    const items = carousel.querySelectorAll(".carousel_item");
+
+    // Disable picture-in-picture for all videos
+    items.forEach(item => {
+        const videos = item.querySelectorAll("video");
+        videos.forEach(video => {
+            video.disablePictureInPicture = true;
+            video.controlsList = "nodownload";
+        });
+    });
+    
+    // Populate the carousel navigation bar with the buttons
+    const leftButtonHtml = `<span class="carousel_arrow left">&#129076;</span>`;
+    const rightButtonHtml = `<span class="carousel_arrow right">&#129078;</span>`;
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel_button"></span>`;
+    });
+    
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel_nav">
+            ${ leftButtonHtml }
+            ${ buttonsHtml.join("") }
+            ${ rightButtonHtml }
+        </div>
+    `);
+
+
+    const buttons = carousel.querySelectorAll(".carousel_button");
+    
+    const leftArrow = document.querySelector(".left");
+    leftArrow.addEventListener("click", function() {
+        if (--indices[carousel] < 0)
+            indices[carousel] = buttons.length - 1;
+        setCarouselItem(carousel, items, buttons, indices[carousel]);
+    });
+
+    const rightArrow = carousel.querySelector(".right");
+    rightArrow.addEventListener("click", function() {
+        if (++indices[carousel] >= buttons.length)
+            indices[carousel] = 0;
+        setCarouselItem(carousel, items, buttons, indices[carousel]);
+    });
+    
+    
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            setCarouselItem(carousel, items, buttons, i);
+        });
+    });
+    
+    // Select the first item on page load
+    //setCarouselItem(carousel, items, buttons, 0);
+    setCarouselItem(carousel, items, buttons, (randomize(items)));
+});
+
+// Randomizer
+function randomize(elements) {
+    return Math.floor(Math.random() * elements.length);
+}
+
+
+// Carousel : youtube.com/watch?=XtFlpgaLbZ4
+// Hide Video UI : https://stackoverflow.com/questions/23279899/hide-video-controls-until-user-hover-over-video
