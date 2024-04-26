5278aec10a745666a1a2e520405de829a4697711
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2024-04-25 23:37:44 -0400

    Updated entries to be displayed on portfolio
    Updated the experiences that will be shown on my portfolio.  Temporarily excluded ACG project for now while I finish up the page.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,4 +1,5 @@
 var indices = new Object();
+var manualCycle = false;
 
 function setCarouselItem(carousel, items, buttons, i) {
     // un-select all items
@@ -43,8 +44,13 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     // Populate the carousel navigation bar with the buttons
     const leftButtonHtml = `<span class="carousel_arrow left">&#129076;</span>`;
     const rightButtonHtml = `<span class="carousel_arrow right">&#129078;</span>`;
-    const buttonsHtml = Array.from(items, () => {
-        return `<span class="carousel_button"></span>`;
+    const buttonsHtml = Array.from(items, (item) => {
+        const images = item.querySelectorAll("img");
+        
+        if (images.length == 0)
+            return `<span class="carousel_button"></span>`;
+        
+        return `<img src="` + images[0].src + `" width="20px" class="carousel_button"></image>`;
     });
     
     carousel.insertAdjacentHTML("beforeend", `
@@ -63,6 +69,7 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         if (--indices[carousel] < 0)
             indices[carousel] = buttons.length - 1;
         setCarouselItem(carousel, items, buttons, indices[carousel]);
+        manualCycle = true;
     });
 
     const rightArrow = carousel.querySelector(".right");
@@ -70,18 +77,20 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         if (++indices[carousel] >= buttons.length)
             indices[carousel] = 0;
         setCarouselItem(carousel, items, buttons, indices[carousel]);
+        manualCycle = true;
     });
     
     
     buttons.forEach((button, i) => {
         button.addEventListener("click", () => {
             setCarouselItem(carousel, items, buttons, i);
+            manualCycle = true;
         });
     });
     
     // Select the first item on page load
-    //setCarouselItem(carousel, items, buttons, 0);
-    setCarouselItem(carousel, items, buttons, (randomize(items)));
+    setCarouselItem(carousel, items, buttons, 0);
+    //setCarouselItem(carousel, items, buttons, (randomize(items)));
 });
 
 // Randomizer
@@ -92,3 +101,26 @@ function randomize(elements) {
 
 // Carousel : youtube.com/watch?=XtFlpgaLbZ4
 // Hide Video UI : https://stackoverflow.com/questions/23279899/hide-video-controls-until-user-hover-over-video
+
+const delayInMilliseconds = 5000; // 2-second delay
+
+function advanceAutoCycle() {
+    setTimeout(function() {
+        if (manualCycle)
+            return;
+
+        document.querySelectorAll(".carousel").forEach(carousel => {
+            const items = carousel.querySelectorAll(".carousel_item");
+            const buttons = carousel.querySelectorAll(".carousel_button");
+
+            if (++indices[carousel] >= buttons.length)
+                indices[carousel] = 0;
+            setCarouselItem(carousel, items, buttons, indices[carousel]);
+        });
+        
+        if (!manualCycle)
+            advanceAutoCycle();
+    }, delayInMilliseconds);
+}
+
+advanceAutoCycle();
